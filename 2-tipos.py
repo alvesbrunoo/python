4d8fc2e076d262c95acefafa75e11079b5b9bebf
alvesdesouzabruno <151651480+alvesbrunoo@users.noreply.github.com>
2024-04-16 12:17:06 -0300

    programando em python

---
+++ 2-tipos.py
@@ -0,0 +1,14 @@
+name = 'Resedent Evil 4 Remake'
+year = 2023
+gamePrice = 300.00
+planIncluded = False
+
+print(name)
+print(year)
+print(gamePrice)
+print(planIncluded)
+
+print(type(name))
+print(type(year))
+print(type(gamePrice))
+print(type(planIncluded))
---
+++ 3-input.py
@@ -0,0 +1,13 @@
+name = input('Digite o nome do jogo:\n ')
+year = int(input('Digite o ano de lançamento do jogo:\n '))
+gamePrice = float(input('Digite o preço do jogo:\n '))
+planIncluded = input('Está incluso no serviço mensal? ').
+
+print(f'O nome do jogo é {name}')
+print(f'O ano de lançamento é {year}')
+print(f'O preço do jogo é {gamePrice}')
+
+if planIncluded == 'True':
+    print('Está incluso no serviço mensal')
+if planIncluded == 'False': 
+    print('Não está incluso no serviço mensal')
