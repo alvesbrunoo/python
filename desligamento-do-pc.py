053c7b226ae9ca815d6bd4334c326e88b9a3f5a8
alvesdesouzabruno <151651480+alvesbrunoo@users.noreply.github.com>
2024-05-22 08:48:49 -0300

    Update desligamento-do-pc.py

--- desligamento-do-pc.py
+++ desligamento-do-pc.py
@@ -1,19 +1,21 @@
 import os
 
+# Desliga o pc em 60s
+def offS():
+    os.system('shutdown /s')
 # Desliga o pc imediatamente
 def off():
     os.system('shutdown /s /t 0')
     
 # # Cancelando o desligamento
-# def cancel():
-#     os.system('shutdown/a')
+def cancel():
+    os.system('shutdown/a')
     
 # # Desliga em 1h
-# def oneHour():
-#     os.system('sutdown/s t3600')
+def oneHour():
+    os.system('sutdown/s t3600')
     
 # # Deslliga em 30min
-# def hafAnHour():
-#     os.system('shoutdown/s/t 180
+def hafAnHour():
+    os.system('shoutdown/s/t 180
 
-off()
