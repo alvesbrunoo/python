6fddda2549bc1b68c7c49d12533602eda23659df
alvesdesouzabruno <151651480+alvesbrunoo@users.noreply.github.com>
2024-05-22 17:41:38 -0300

    statistics

---
+++ 28-statistic.py
@@ -0,0 +1,11 @@
+import statistics
+
+# 1- Aplicar a média
+print(statistics.mean([3, 2, 3, 8, 9]))
+
+# 2 - Aplicar mediana 
+print(statistics.median([1, 2, 4, 8, 9]))
+print(statistics.median([1, 2, 3, 7, 8, 9]))
+
+# 3 - Aplicar moda 
+print(statistics.stdev([1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5]))
